49031d359d365dcba9ab5a7cb050a1bb9c44f20e
Andriy Nikulytsya <anikulytsya@spectrumdt.com>
2018-05-28 09:15:17 -0400

    - use swagger 3

--- apps/templ/src/main/java/com/anik/swagger/BootstrapSwagger.java
+++ apps/templ/src/main/java/com/anik/swagger/BootstrapSwagger.java
@@ -1,21 +1,56 @@
 package com.anik.swagger;
 
-import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.v3.jaxrs2.integration.JaxrsOpenApiContextBuilder;
+import io.swagger.v3.oas.integration.OpenApiConfigurationException;
+import io.swagger.v3.oas.integration.SwaggerConfiguration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
 public class BootstrapSwagger extends HttpServlet {
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
 
-        BeanConfig beanConfig = new BeanConfig();
-        beanConfig.setVersion("1.0.2");
-        beanConfig.setSchemes(new String[]{"http"});
-        beanConfig.setHost("localhost:8002");
-        beanConfig.setBasePath("/api");
-        beanConfig.setResourcePackage("io.swagger.resources");
-        beanConfig.setScan(true);
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		OpenAPI oas = new OpenAPI();
+		Info info = new Info()
+				  .title("Swagger Sample App - independent config exposed by dedicated servlet")
+				  .description("This is a sample server Petstore server.  You can find out more about Swagger "
+							 + "at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).  For this sample, "
+							 + "you can use the api key `special-key` to test the authorization filters.")
+				  .termsOfService("http://swagger.io/terms/")
+				  .contact(new Contact()
+							 .email("apiteam@swagger.io"))
+				  .license(new License()
+							 .name("Apache 2.0")
+							 .url("http://www.apache.org/licenses/LICENSE-2.0.html"));
+
+		oas.info(info);
+		SwaggerConfiguration oasConfig = new SwaggerConfiguration()
+				  .openAPI(oas)
+				  .resourcePackages(Stream.of("com.anik").collect(Collectors.toSet()));
+
+		// or
+/*
+    try {
+      new GenericOpenApiContext().openApiConfiguration(oasConfig).init();
+    } catch (OpenApiConfigurationException e) {
+      e.printStackTrace();
     }
+		 */
+		try {
+			new JaxrsOpenApiContextBuilder()
+					  .servletConfig(config)
+					  //.ctxId(config.getServletName())
+					  .openApiConfiguration(oasConfig)
+					  .buildContext(true);
+		} catch (OpenApiConfigurationException e) {
+			throw new ServletException(e.getMessage(), e);
+		}
+	}
 }
