e53e48e0f653486d3e06d1701ae05aea133d1838
Andriy Nikulytsya <anikulytsya@spectrumdt.com>
2018-06-22 23:05:48 -0400

    - ignore missing fields, empty and null values in json serializer/deserializer

--- apps/templ/src/main/java/com/anik/provider/JacksonObjectMapperProvider.java
+++ apps/templ/src/main/java/com/anik/provider/JacksonObjectMapperProvider.java
@@ -1,5 +1,7 @@
 package com.anik.provider;
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import javax.ws.rs.ext.ContextResolver;
@@ -7,22 +9,24 @@ import javax.ws.rs.ext.Provider;
 
 @Provider
 public class JacksonObjectMapperProvider implements ContextResolver<ObjectMapper> {
-    final ObjectMapper defaultObjectMapper;
+	final ObjectMapper defaultObjectMapper;
 
-    public JacksonObjectMapperProvider() {
-        defaultObjectMapper = createDefaultMapper();
-    }
+	public JacksonObjectMapperProvider() {
+		defaultObjectMapper = createDefaultMapper();
+	}
 
-    @Override
-    public ObjectMapper getContext(Class<?> type) {
-        return defaultObjectMapper;
-    }
+	@Override
+	public ObjectMapper getContext(final Class<?> type) {
+		return defaultObjectMapper;
+	}
 
-    private static ObjectMapper createDefaultMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-		   mapper.enable(SerializationFeature.INDENT_OUTPUT);
-       // mapper.registerModule(new ObjectIdSerializerModule());
+	private static ObjectMapper createDefaultMapper() {
+		final ObjectMapper mapper = new ObjectMapper();
 
-        return mapper;
-    }
+		mapper.enable(SerializationFeature.INDENT_OUTPUT);
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		mapper.setSerializationInclusion(Include.NON_NULL);
+		mapper.setSerializationInclusion(Include.NON_EMPTY);
+		return mapper;
+	}
 }
