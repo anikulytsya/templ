2be3ab089cf043723ac75e75c30f3009d9db2b27
Andriy Nikulytsya <anikulytsya@spectrumdt.com>
2018-06-25 10:23:23 -0400

    - configure ReaderListener to be used

--- apps/templ/src/main/java/com/anik/swagger/OpenApiUpdater.java
+++ apps/templ/src/main/java/com/anik/swagger/OpenApiUpdater.java
@@ -4,9 +4,11 @@ import io.swagger.v3.jaxrs2.Reader;
 import io.swagger.v3.jaxrs2.ReaderListener;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.OpenAPI;
+import javax.ws.rs.Path;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Path("")
 @OpenAPIDefinition
 public class OpenApiUpdater implements ReaderListener {
 	private static final Logger LOG = LoggerFactory.getLogger(OpenApiUpdater.class);
